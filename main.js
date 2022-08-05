5ba389af9bd7f6530bf58dc0e0c7c1e8b5ef9e79
Hui Wang <xeraonomen@gmail.com>
2022-08-05 11:28:33 -0400

    added popup

---
+++ main.js
@@ -0,0 +1,30 @@
+const projects = document.getElementsByClassName("projects");
+const projectTitles = document.getElementsByClassName("project_titles");
+const projectImages = document.getElementsByClassName("project_images");
+const closeModalButton = document.getElementById("close_button");
+const overlay = document.getElementById("overlay");
+const modal = document.getElementById("project_popup");
+
+function openModal() {
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal() {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+closeModalButton.onclick = closeModal;
+overlay.onclick = closeModal;
+
+for (let i = 0; i < projects.length; i++) {
+    let project = projects[i];
+    let projectTitle = projectImages[i];
+    let projectImage = projectImages[i];
+    let popupTitle = document.getElementById("popup_title");
+    let popupImage = document.getElementById("popup_img");
+    popupTitle.innerHTML = projectTitle.innerHTML;
+    popupImage.src = projectImage.src;
+    project.onclick = openModal;
+}
