3069eaf91610a84c15f701d0c5deec3c54a3a1c1
Hui Wang <xeraonomen@gmail.com>
2022-08-05 13:02:51 -0400

    finished popup

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ const overlay = document.getElementById("overlay");
 const modal = document.getElementById("project_popup");
 
 function openModal() {
+    let popupTitle = document.getElementById("popup_title");
+    let popupImage = document.getElementById("popup_img");
+    let popupRepo = document.getElementById("popup_repo_link");
+    let popupPages = document.getElementById("popup_pages_link");
+
+    popupTitle.innerHTML = this.children[1].innerHTML;
+    popupImage.src = this.children[0].src;
+    popupRepo.href = this.children[2].innerHTML;
+    popupPages.href = this.children[3].innerHTML;
+    
     modal.classList.add('active');
     overlay.classList.add('active');
 }
@@ -19,16 +29,6 @@ function closeModal() {
 closeModalButton.onclick = closeModal;
 overlay.onclick = closeModal;
 
-for (let i = 0; i < projects.length; i++) {
-    let project = projects[i];
-    let projectTitle = projectTitles[i];
-    let projectImage = projectImages[i];
-
-    let popupTitle = document.getElementById("popup_title");
-    let popupImage = document.getElementById("popup_img");
-
-    popupTitle.innerHTML = projectTitle.innerHTML;
-    popupImage.src = projectImage.src;
-
+for (let project of projects) {
     project.onclick = openModal;
 }
