05c4f0e3f6ad40550c8df568a3e46eef20df302b
Hui Wang <xeraonomen@gmail.com>
2022-08-05 12:02:17 -0400

    capitalized nav bar

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const projects = document.getElementsByClassName("projects");
 const projectTitles = document.getElementsByClassName("project_titles");
 const projectImages = document.getElementsByClassName("project_images");
+
 const closeModalButton = document.getElementById("close_button");
 const overlay = document.getElementById("overlay");
 const modal = document.getElementById("project_popup");
@@ -20,11 +21,14 @@ overlay.onclick = closeModal;
 
 for (let i = 0; i < projects.length; i++) {
     let project = projects[i];
-    let projectTitle = projectImages[i];
+    let projectTitle = projectTitles[i];
     let projectImage = projectImages[i];
+
     let popupTitle = document.getElementById("popup_title");
     let popupImage = document.getElementById("popup_img");
+
     popupTitle.innerHTML = projectTitle.innerHTML;
     popupImage.src = projectImage.src;
+
     project.onclick = openModal;
 }
